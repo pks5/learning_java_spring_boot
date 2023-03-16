9704cfdf7e6566ba62eceb2cc81d5c02993b7ab7
pk <pk@pksoftware.de>
2023-03-16 19:36:14 +0100

    Template

---
+++ tutorials/01_spring_boot_template/src/main/java/de/pksoftware/learning/java_spring_boot/template/MySpringBootApplication.java
@@ -0,0 +1,13 @@
+package de.pksoftware.learning.java_spring_boot.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootApplication.class, args);
+	}
+
+}
